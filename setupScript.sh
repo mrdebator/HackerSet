94451606e7d1f4675ac4fbf404c799314fe01365
ac3965 <ac3965@cs.drexel.edu>
2020-06-22 02:29:17 -0400

    added impacket, dirsearch, ruby, java, git, mlocate, python, pip, masscan

--- setupScript.sh
+++ setupScript.sh
@@ -1,24 +1,84 @@
 #! /bin/bash
 # Author: Ansh Chandnani (mrdebator)
 
-# update current libraries 
+# Unzipping rockyou.txt
+unzip ./wordlists/rockyou.txt.zip  -d ./wordlists
+
+# update current libraries
 echo 'Updating current libraries...'
 sudo apt-get update
 
-# Aircrack-ng 
+# net-tools
+echo 'Installing net-tools'
+sudo apt install -y net-tools
+echo 'Upgrading net-tools'
+sudo apt upgrade net-tools
+
+# cURL
+echo 'Installing cURL...'
+sudo apt install -y curl
+
+# Python
+echo 'Installing Python 2.7...'
+sudo apt install -y python2
+sudo apt upgrade python2
+
+echo 'Installing Python 3'
+sudo apt install -y python3
+sudo apt upgrade python3
+
+# pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+echo 'Configuring pip2...'
+sudo python2 get-pip.py
+echo 'Configuring pip3...'
+sudo python3 get-pip.py   # This sets pip3 as default for pip
+
+# Java
+echo 'Installing Java...'
+sudo apt-get install -y openjdk-8-jdk
+sudo apt install -y default-jre
+
+# Ruby
+echo 'Installing Ruby...'
+sudo apt install -y ruby-full
+
+# git
+echo 'Installing Git...'
+sudo apt install -y git
+
+# mlocate
+echo 'Installing mlocate and locate database...'
+sudo apt-get install -y mlocate
+sudo updatedb
+
+# masscan
+echo 'Installing masscan for fast Internet scanning'
+sudo apt-get install -y masscan
+echo 'Upgrading masscan...'
+sudo apt upgrade masscan
+
+# Aircrack-ng
 echo 'Installing Aircrack-ng...'
 sudo apt-get install -y aircrack-ng
 echo 'Upgrading Aircrack-ng...'
 sudo apt-get upgrade aircrack-ng
 
-# JohnTheRipper
+# Dependancies for John Jumbo
+sudo apt-get install -y build-essential libssl-dev   # REQUIRED OpenSSL component
+# Other reccpmmended dependancies (https://openwall.info/wiki/john/tutorials/Ubuntu-build-howto)
+sudo apt-get install -y yasm libgmp-dev libpcap-dev libnss3-dev libkrb5-dev pkg-config libbz2-dev zlib1g-dev
+
+# JohnTheRipper Jumbo
 echo 'Installing JohnTheRipper...'
-sudo apt-get install -y john
-echo 'Upgrading JohnTheRipper...'
-sudo apt-get upgrade john
+# Cloning
+git clone git://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo john-jumbo
+# Building
+cd ./john-jumbo/src
+./configure && make -s clean && make -sj4
 
 # Hashcat
-sudo apt-get install ocl-icd-libopencl1 git build-essential
+sudo apt-get install -y ocl-icd-libopencl1 git build-essential
 mkdir /installs && mkdir /installs/apps && cd /installs/apps
 
 git clone https://github.com/hashcat/hashcat.git
@@ -26,7 +86,7 @@ git clone https://github.com/hashcat/hashcat.git
 cd hashcat
 git submodule update -init
 
-make 
+make
 cd ../
 
 git clone https://github.com/hashcat/hashcat-utils.git
@@ -49,3 +109,13 @@ sudo apt-get install -y tshark
 echo 'Upgrading tShark...'
 sudo apt-get upgrade tshark
 
+# dirsearch
+echo 'Installing dirsearch...'
+git clone https://github.com/mausoria/dirsearch.git
+
+# Impacket
+git clone https://github.com/SecureAuthCorp/impacket.git
+cd impacket
+pip install .
+cd ..
+sudo apt-get install -y python3-impacket
